c5c8ec871184376de70dc6e9191885271cae67f4
Ibiere <103963379+Fyngirl@users.noreply.github.com>
2023-02-16 04:47:09 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/*
- * main - print a line of code using puts
+/**
+ * main -Entry point print a line of code using puts
  *
- * Return: 0
+ * Return:always 0
  */
 int main(void)
 {
