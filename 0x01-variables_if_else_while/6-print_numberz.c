ea904b0b4c10d0ab10685b34a6e44f0472aee28d
Ibiere <103963379+Fyngirl@users.noreply.github.com>
2023-02-17 11:01:42 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - a program that prints all single digits
+ * Return: 0 successfully
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+	return (0);
+}
