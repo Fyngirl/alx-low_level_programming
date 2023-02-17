26e42d9e8c0aa59e66343b2d809bf4d23983f63b
Ibiere <103963379+Fyngirl@users.noreply.github.com>
2023-02-17 11:09:40 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio>
+/**
+ * main - A program that prints the lowercase alphabet
+ * Return: 0 Successfully
+ */
+int main(void)
+{
+	char alphabet = 'z';
+
+	while (alphabet <= 'a')
+	{
+		putchar(alphabet);
+
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
