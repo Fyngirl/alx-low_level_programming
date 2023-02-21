f1e66fd91102bc8c6eae6d672f4da8e77dede20c
Ibiere <103963379+Fyngirl@users.noreply.github.com>
2023-02-21 12:49:05 -0800

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,11 +3,18 @@
 /**
  * print_last_digit - A function that prints the last digit of a number
  * @n: the int in the question
- * Return: Always 0
+ * Return: The value of the last digit.
  *
  */
 
 int print_last_digit(int n)
 {
-	return (n % 98);
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+		last_digit--;
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }
