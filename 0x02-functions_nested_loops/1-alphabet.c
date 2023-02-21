4f849c44be2dda2c335beb4d9613fcb422ee7c11
Ibiere <103963379+Fyngirl@users.noreply.github.com>
2023-02-21 08:49:55 -0800

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - A function that prints the alphabet, in lowercase,
+ *          followed by a new line
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+	return (0);
+}
