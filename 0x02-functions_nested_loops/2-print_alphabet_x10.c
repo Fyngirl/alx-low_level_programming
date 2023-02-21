87c9738569ce5b3ff859c91ef3731d94886e62aa
Ibiere <103963379+Fyngirl@users.noreply.github.com>
2023-02-21 11:02:31 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,13 @@
  */
 void print_alphabet_x10(void)
 {
-	char Letter;
 	int count = 0;
+	char Letter;
 
 	while (count++ <= 9)
 	{
-
-	for (Letter = 'a'; Letter <= 'z'; Letter++)
-
-		_putchar(Letter);
+		for (Letter = 'a'; Letter <= 'z'; Letter++)
+			_putchar(Letter);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
