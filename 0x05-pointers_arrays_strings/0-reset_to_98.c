5e27870c31d1691812969d242339ea9af61b2810
Ibiere <103963379+Fyngirl@users.noreply.github.com>
2023-02-27 10:08:31 -0800

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - A function that takes a pointer to int as parameter
+ * @n: int to check
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a project for pointers, arrays and strings
