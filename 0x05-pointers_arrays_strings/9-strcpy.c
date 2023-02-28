0e31600c419e6d0630267bc7fc73ee3b231e3765
Ibiere <103963379+Fyngirl@users.noreply.github.com>
2023-02-28 12:13:05 -0800

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,8 +12,8 @@ char *_strcpy(char *dest, char *src)
 {
 	int a;
 
-	for (a = 0; scr[a] != '\0'; a++)
-		dest[a] = scr[a];
+	for (a = 0; src[a] != '\0'; a++)
+		dest[a] = src[a];
 	dest[a] = '\0';
 	return (dest);
 }
