6b56b45ab5d771ed010925bc1537f17371067f0e
Ibiere <103963379+Fyngirl@users.noreply.github.com>
2023-02-28 12:09:51 -0800

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copy the string pointed to by src
+ * @dest: character to check
+ * @src: character to check
+ * Return: Always 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; scr[a] != '\0'; a++)
+		dest[a] = scr[a];
+	dest[a] = '\0';
+	return (dest);
+}
