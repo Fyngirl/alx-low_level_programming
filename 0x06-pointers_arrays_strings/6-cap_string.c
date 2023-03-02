f60502e005dd9afd6fa4d12ee4793f291b680bd3
Ibiere <103963379+Fyngirl@users.noreply.github.com>
2023-03-02 08:29:34 -0800

    6-cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string
+ * @s: string to be capitalized
+ * Return: s
+ */
+
+char *cap_string(char *s)
+{
+	int a = 0, b;
+	char c[] = " \t\n,;.!?\"(){}";
+
+	while (*(s + a))
+	{
+		if (*(s + a) >= 'a' && *(s + i) <= 'z')
+		{
+			if (a == 0)
+				*(s + a) -= 'a' - 'A';
+			else
+			{
+				for (b = 0; b <= 12; b++)
+				{
+				if (c[b] == *(s + a - 1))
+				*(s + a) -= 'a' - 'A';
+				}
+			}
+		}
+	a++
+	}
+return (s);
+}
