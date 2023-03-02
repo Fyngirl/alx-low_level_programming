03bbe9b3c3c71badbf098fb5650570e00a479f71
Ibiere <103963379+Fyngirl@users.noreply.github.com>
2023-03-02 08:43:27 -0800

    7-leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: string to encode
+ * Return: address s
+ */
+
+char *leet(char *s)
+{
+	int a, b;
+	char c[] = "aAeEoOtTlL";
+	char d[] = "4433007711";
+
+	for (a = 0; *(s + a); a++)
+	{
+		for (b = 0; b <= 9; a++)
+		{
+			if (c[b] == s[a])
+				s[a] = d[b];
+		}
+	}
+	return (s);
+}
