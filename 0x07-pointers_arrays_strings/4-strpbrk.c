bd480ced1079d1f7aaed8d82d74a96cf428b830e
Ibiere <103963379+Fyngirl@users.noreply.github.com>
2023-03-06 13:20:06 -0800

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - A function that searches a string
+ * @s: The string to be located
+ * @accept: the string
+ * Return: returns a pointer to the byte
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	char *p = accept;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; p[i]; p++)
+		{
+			if (*s == p[i])
+				return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
