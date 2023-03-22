5ec4c4b18e9e088f7f6e3582cf19ae089cf7fd69
Ibiere <103963379+Fyngirl@users.noreply.github.com>
2023-03-22 12:47:24 -0700

    2-int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,6 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
+	if (array != NULL && cmp != NULL)
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
@@ -25,4 +26,3 @@ int int_index(int *array, int size, int (*cmp)(int))
 	}
 	return (-1);
 }
-
