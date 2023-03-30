4c3a4f418ff95c5e3c20fcabff79948c1eaafb92
Ibiere <103963379+Fyngirl@users.noreply.github.com>
2023-03-30 10:18:28 -0700

    2-add_node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -38,7 +38,7 @@ list_t *add_node(list_t **head, const char *str)
 	}
 	new_node->str = strdup(str);
 
-	new_node = _strlen(str);
+	new_node->len = _strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 
