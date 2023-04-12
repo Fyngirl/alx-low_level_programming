5b3e4332d23978b43aa064678fb9fc8a7944ce23
Ibiere <103963379+Fyngirl@users.noreply.github.com>
2023-04-12 01:30:21 -0700

    0-read_textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,60 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of file to be checked
+ * @letters: to be read
+ * Return: 0 if NULL
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t bytes_read, by_writ;
+	int fd;
+	char *buffer;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	buffer = malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
+	{
+		return (0);
+	}
+
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+
+	buffer[bytes_read] = '\0';
+
+	by_writ = write(STDOUT_FILENO, buffer, bytes_read);
+	if (by_writ != bytes_read)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+
+	close(fd);
+
+	free(buffer);
+
+	return (bytes_read);
+}
